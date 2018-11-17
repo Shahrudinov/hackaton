a31ecf530587b4762f32106fda7bff21a468792d
Kirill Litvinov <themaiby0@gmail.com>
2018-11-17 10:33:19 +0200

    users contacts

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password', 'phone_number', 'skype'
     ];
 
     /**
